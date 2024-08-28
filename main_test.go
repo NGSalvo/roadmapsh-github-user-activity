d2cc72bfab4b8f19b1f654abcbf75e9ea1980ccb
Nicolás Salvo <nicosalvo@gmail.com>
2024-08-28 04:22:56 +0000

    test: fetch recent activity and fail to fetch inexistent user

--- main_test.go
+++ main_test.go
@@ -14,5 +14,13 @@ func TestMain(t *testing.T) {
 
 		asserts.NoError(err)
 		asserts.NotEmpty(result)
+		asserts.IsType([]*Event{}, result)
+	})
+
+	t.Run("Should fail to fetch the recent activity of a GitHub user", func(t *testing.T) {
+		result, err := GetRecentActivity("inexistent-user")
+
+		asserts.Error(err)
+		asserts.Empty(result)
 	})
 }
